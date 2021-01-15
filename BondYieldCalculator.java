f245600b326de6b082e879130daa53d8e2cd64b7
Kah Soon Yap <kahsoonyap1998@hotmail.com>
2021-01-14 20:04:22 -0500

    Initial commit

---
+++ BondYieldCalculator.java
@@ -0,0 +1,13 @@
+public class BondYieldCalculator {
+  public BondYieldCalculator() {
+  }
+
+  public double CalcPrice(double coupon, int years, double face, double rate) {
+    return 0.0;
+  }
+
+  public double CalcYield(double coupon, int years, double face, double price) {
+    return 0.0;
+  }
+
+}
---
+++ Driver.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Driver {
+  public static void main(String[] args) {
+    System.out.println("Chimera - Bond Yield Calculator");
+    BondYieldCalculator calc1 = new BondYieldCalculator();
+  }
+
+
+}
