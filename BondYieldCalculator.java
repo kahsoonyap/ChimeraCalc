9fc9b218ec3f5b6df22496c2bf455b248ffacfae
Kah Soon Yap <kahsoonyap1998@hotmail.com>
2021-01-14 22:18:29 -0500

    basic, direct translation of equation into code

--- BondYieldCalculator.java
+++ BondYieldCalculator.java
@@ -1,13 +1,50 @@
+import java.lang.Math;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BondYieldCalculator {
+  private static final int DECIMAL_ACCURACY = 7;
   public BondYieldCalculator() {
   }
 
   public double CalcPrice(double coupon, int years, double face, double rate) {
-    return 0.0;
+    double principalPayment = calcPrincipalPayment(years, face, rate);
+    double totalCouponPayment = calcTotalCouponPayment(coupon, years, face, rate);
+    BigDecimal price = new BigDecimal(principalPayment + totalCouponPayment);
+    price = price.setScale(DECIMAL_ACCURACY, RoundingMode.HALF_UP);
+
+    return price.doubleValue();
   }
 
   public double CalcYield(double coupon, int years, double face, double price) {
     return 0.0;
+  }
+
+
+
+  private double calcDiscount(int year, double rate) {
+    return Math.pow((1.0 + rate), year);
+  }
+
+  private double calcCoupon(double coupon, double face) {
+    return coupon * face;
+  }
+
+  private double calcTotalCouponPayment(double coupon, int years, double face, double rate) {
+    double total = 0;
+    double couponPayment = calcCoupon(coupon, face);
+
+    for (int year = 1; year <= years; year++) {
+      total += couponPayment / calcDiscount(year, rate);
+    }
+
+    return total;
+  }
+
+  private double calcPrincipalPayment(int years, double face, double rate) {
+    double discount = calcDiscount(years, rate);
+
+    return face / discount;
   }
 
 }
--- Driver.java
+++ Driver.java
@@ -1,10 +1,35 @@
 import java.util.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Driver {
   public static void main(String[] args) {
-    System.out.println("Chimera - Bond Yield Calculator");
-    BondYieldCalculator calc1 = new BondYieldCalculator();
+    System.out.println("Chimera - Bond Yield Calculator\n");
+    BondYieldCalculator calc = new BondYieldCalculator();
+
+    System.out.println("=====================  Calc Price  =====================");
+    // coupon, years, face, rate
+    printResults(calc.CalcPrice(0.10, 5, 1000, 0.15), 832.3922451);
+    printResults(calc.CalcPrice(0.15, 5, 1000, 0.15), 1000.0000000);
+    printResults(calc.CalcPrice(0.10, 5, 1000, 0.08), 1079.8542007);
+
+    printResults(calc.CalcPrice(0.0, 10, 500, 0.10), 192.7716447);
+    printResults(calc.CalcPrice(0.10, 10, 500, 0.0), 1000.0000000);
+    printResults(calc.CalcPrice(0.10, 0, 500, 0.10), 500.0000000);
+    printResults(calc.CalcPrice(0.10, 10, 0.0, 0.10), 0.0000000);
+
+
   }
 
+  public static void printResults(double result, double expected) {
+    if (result == expected) {
+      System.out.print("O\t");
+    } else{
+      System.out.print("X\t");
+    }
+    BigDecimal res = new BigDecimal(result);
+    BigDecimal exp = new BigDecimal(expected);
+    System.out.printf("Result: %13s\tExpected: %13s\n", res.setScale(7, RoundingMode.HALF_UP), exp.setScale(7, RoundingMode.HALF_UP));
+  }
 
 }
