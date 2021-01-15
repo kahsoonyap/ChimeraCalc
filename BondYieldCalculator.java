14517e96934af05e02f9ee0c1e45c42927dd39ee
Kah Soon Yap <kahsoonyap1998@hotmail.com>
2021-01-15 07:27:06 -0500

    adding in memo

--- BondYieldCalculator.java
+++ BondYieldCalculator.java
@@ -1,38 +1,71 @@
+import java.util.*;
 import java.lang.Math;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class BondYieldCalculator {
   private static final int DECIMAL_ACCURACY = 7;
+  private HashMap<List, HashMap<Integer, Double>> memo;
   public BondYieldCalculator() {
-  }
-
-  public double CalcPrice(double coupon, int years, double face, double rate) {
-    double principalPayment = calcPrincipalPayment(years, face, rate);
-    double totalCouponPayment = calcTotalCouponPayment(coupon, years, face, rate);
-    BigDecimal price = new BigDecimal(principalPayment + totalCouponPayment);
-    price = price.setScale(DECIMAL_ACCURACY, RoundingMode.HALF_UP);
-
-    return price.doubleValue();
+    // face -> coupon -> rate -> price
+    // HashMap<double, HashMap<double, <HashMap<double, HashMap<int, double>>>>> memo = new HashMap<>();
+    memo = new HashMap<List, HashMap<Integer, Double>>();
   }
 
   // http://www.columbia.edu/~ks20/FE-Notes/4700-07-Notes-bonds.pdf
   // https://financeformulas.net/Yield_to_Maturity.html
   // http://phillipmfeldman.org/Python/roots/find_roots.html#:~:text=Abstract,the%20Newton%2DRaphson%20method).
+  // https://www.sciencedirect.com/science/article/pii/S0893965903901194#:~:text=Standard%20text%20books%20in%20numerical,the%20secant%20method%20becomes%20linear.
   public double CalcYield(double coupon, int years, double face, double price) {
-    BigDecimal yield = new BigDecimal((calcCoupon(coupon, face) + (face - price) / years) / ((face + price) / 2));
+    // BigDecimal yield = new BigDecimal((calcCoupon(coupon, face) + (face - price) / years) / ((face + price) / 2));
+    // yield = yield.setScale(DECIMAL_ACCURACY, RoundingMode.HALF_UP);
+    // return yield.doubleValue();
+    double guessA = Double.MAX_VALUE;
+    double guessB = Double.MIN_VALUE;
+    double guessC = 0;
+
+    double priceA = calcPriceHelper(coupon, years, face, guessA);
+    double priceB = 0;
+
+    while (Math.abs(price - priceA) > 0.0000001) {
+      priceB = calcPriceHelper(coupon, years, face, guessB);
+      guessC = guessA - priceA * (guessB - guessA) / (priceB - priceA);
+      priceA = calcPriceHelper(coupon, years, face, guessA);
+    }
+
+    BigDecimal yield = new BigDecimal(priceA);
     yield = yield.setScale(DECIMAL_ACCURACY, RoundingMode.HALF_UP);
     return yield.doubleValue();
   }
 
 
+  public double CalcPrice(double coupon, int years, double face, double rate) {
+    double rawDouble = calcPriceHelper(coupon, years, face, rate);
+    BigDecimal price = new BigDecimal(rawDouble);
+    price = price.setScale(DECIMAL_ACCURACY, RoundingMode.HALF_UP);
 
-  private double calcDiscount(int year, double rate) {
-    return Math.pow((1.0 + rate), year);
+    return price.doubleValue();
   }
 
-  private double calcCoupon(double coupon, double face) {
-    return coupon * face;
+  private double calcPriceHelper(double coupon, int years, double face, double rate) {
+    if (!memo.containsKey(Arrays.asList(coupon, face, rate))) {
+      HashMap<Integer, Double> newRecord = new HashMap<Integer, Double>();
+      memo.put(Arrays.asList(coupon, face, rate), newRecord);
+    }
+    HashMap<Integer, Double> record = memo.get(Arrays.asList(coupon, face, rate));
+    if (!record.containsKey(years)) {
+      double principalPayment = calcPrincipalPayment(years, face, rate);
+      double totalCouponPayment = calcTotalCouponPayment(coupon, years, face, rate);
+      record.put(years, principalPayment + totalCouponPayment);
+    }
+
+    return record.get(years);
+  }
+
+  private double calcPrincipalPayment(int years, double face, double rate) {
+    double discount = calcDiscount(years, rate);
+
+    return face / discount;
   }
 
   private double calcTotalCouponPayment(double coupon, int years, double face, double rate) {
@@ -46,10 +79,11 @@ public class BondYieldCalculator {
     return total;
   }
 
-  private double calcPrincipalPayment(int years, double face, double rate) {
-    double discount = calcDiscount(years, rate);
-
-    return face / discount;
+  private double calcDiscount(int year, double rate) {
+    return Math.pow((1.0 + rate), year);
   }
 
+  private double calcCoupon(double coupon, double face) {
+    return coupon * face;
+  }
 }
--- Driver.java
+++ Driver.java
@@ -4,11 +4,15 @@ import java.math.RoundingMode;
 
 public class Driver {
   public static void main(String[] args) {
+    long startTime;
+    long endTime;
+
     System.out.println("Chimera - Bond Yield Calculator\n");
     BondYieldCalculator calc = new BondYieldCalculator();
 
     System.out.println("=====================  Calc Price  =====================");
     // coupon, years, face, rate
+    startTime = System.currentTimeMillis();
     printResults(calc.CalcPrice(0.10, 5, 1000, 0.15), 832.3922451);
     printResults(calc.CalcPrice(0.15, 5, 1000, 0.15), 1000.0000000);
     printResults(calc.CalcPrice(0.10, 5, 1000, 0.08), 1079.8542007);
@@ -17,13 +21,22 @@ public class Driver {
     printResults(calc.CalcPrice(0.10, 10, 500, 0.0), 1000.0000000);
     printResults(calc.CalcPrice(0.10, 0, 500, 0.10), 500.0000000);
     printResults(calc.CalcPrice(0.10, 10, 0.0, 0.10), 0.0000000);
+    endTime = System.currentTimeMillis();
+    timeDiff(startTime, endTime);
 
-    System.out.println("\n=====================  Calc Yield  =====================");
+    System.out.println("=====================  Calc Yield  =====================");
     // coupon, years, face, price
-    printResults(calc.CalcYield(0.10, 5, 1000, 832.4), 0.1499974);
-    printResults(calc.CalcYield(0.10, 5, 1000, 1000), 0.1000000);
-    printResults(calc.CalcYield(0.10, 5, 1000, 1079.85), 0.080010);
-
+    // printResults(calc.CalcYield(0.10, 5, 1000, 832.4), 0.1499974);
+    // printResults(calc.CalcYield(0.10, 5, 1000, 1000), 0.1000000);
+    // printResults(calc.CalcYield(0.10, 5, 1000, 1079.85), 0.080010);
+    HashMap<List, HashMap<Integer, Double>> memo = new HashMap<List, HashMap<Integer, Double>>();
+    HashMap<Integer, Double> yearPriceMap = new HashMap<Integer, Double>();
+    memo.put(Arrays.asList(0.10, 1000.0, 0.15), yearPriceMap);
+    System.out.println(memo.containsKey(Arrays.asList(0.10, 1000.0, 0.15)));
+    HashMap<Integer, Double> temp = memo.get(Arrays.asList(0.10, 1000.0, 0.15));
+    System.out.println(temp);
+    temp.put(0, 1000.0);
+    System.out.println(memo.get(Arrays.asList(0.10, 1000.0, 0.15)).get(0));
   }
 
   public static void printResults(double result, double expected) {
@@ -37,4 +50,7 @@ public class Driver {
     System.out.printf("Result: %13s\tExpected: %13s\n", res.setScale(7, RoundingMode.HALF_UP), exp.setScale(7, RoundingMode.HALF_UP));
   }
 
+  public static void timeDiff(long startTime, long endTime) {
+    System.out.printf("Execution time: %dms\n\n", endTime - startTime);
+  }
 }
