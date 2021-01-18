27ecbfaebf3ff3c4a4a9f79859864953bdad5652
Kah Soon Yap <kahsoonyap1998@hotmail.com>
2021-01-18 18:35:43 -0500

    added comments and renamed methods/vars to make more sense

--- BondYieldCalculator.java
+++ BondYieldCalculator.java
@@ -52,7 +52,16 @@ public class BondYieldCalculator {
     return guessC;
   }
 
-
+  /**
+  * Calculates the price of a bond given the coupon rate, years to maturity
+  * face value, and the discount rate. Accurate to 10e-7.
+  * (C / (1 + r)) + (C / (1 + r)^2) + ... + (C / (1 + r)^N) + (F / (1 + r)^N)
+  * @param  coupon coupon rate
+  * @param  years  number of years to maturity
+  * @param  face   face value
+  * @param  rate   discount rate
+  * @return        price
+  */
   public double CalcPrice(double coupon, int years, double face, double rate) {
     double rawDouble = calcPriceHelper(coupon, years, face, rate);
     BigDecimal price = new BigDecimal(rawDouble);
@@ -62,56 +71,104 @@ public class BondYieldCalculator {
   }
 
   private double calcPriceHelper(double coupon, int years, double face, double rate) {
-    if (years == 0) {
-      return face;
-    }
-    double principalPayment = calcPrincipalPayment(years, face, rate);
-    double totalCouponPayment = calcTotalCouponPayment(coupon, years, face, rate);
-    return principalPayment + totalCouponPayment;
+    /* Special case when years = 0: return the face value */
+    if (years == 0) { return face; }
+
+    double totalCouponPayment = calcTotalCouponPaymentValue(coupon, years, face, rate);
+    double principalPaymentValue = calcPrincipalPaymentValue(years, face, rate);
+
+    return totalCouponPayment + principalPaymentValue;
   }
 
-  private double calcPrincipalPayment(int years, double face, double rate) {
-    double discount = calcDiscount(years, rate);
+  /**
+  * Calculates the value of a bond's principal payment given the
+  * years to maturity, the face value, and the discount rate.
+  * F / (1 + r)^N
+  * @param  years number of years to maturity
+  * @param  face  face value
+  * @param  rate  discount rate
+  * @return       value of the bond's principal payment
+  */
+  private double calcPrincipalPaymentValue(int years, double face, double rate) {
+    double discount = calcValueModifier(years, rate);
 
     return face / discount;
   }
 
-  private double calcTotalCouponPayment(double coupon, int years, double face, double rate) {
+  /**
+  * Calculates the value of the total coupon payments made for a bond
+  * given the coupon rate, the number of years to maturity,
+  * the face value, and the discount rate.
+  * (C / (1 + r)) + (C / (1 + r)^2) + ... + (C / (1 + r)^N)
+  * @param  coupon coupon rate
+  * @param  years  number of years to maturity
+  * @param  face   face value
+  * @param  rate   discount rate
+  * @return        value of coupon payment total
+  */
+  private double calcTotalCouponPaymentValue(double coupon, int years, double face, double rate) {
     List key = Arrays.asList(coupon, face, rate);
     double cf = calcCF(coupon, face);
 
+    /*
+     * Check if combination of coupon, face value, and rate
+     * have already been seen. If not, create an entry into
+     * the memo with said combination.
+     */
     if (!couponMemo.containsKey(key)) {
       HashMap newRecord = new HashMap<Integer, Double>();
-      newRecord.put(0, 0.0);
+      newRecord.put(0, 0.0);            /* Base case for later calculations */
       couponMemo.put(key, newRecord);
     }
 
     HashMap record = couponMemo.get(key);
-    return calcCouponPayment(cf, years, rate, record);
+    return calcCouponPaymentValue(cf, years, rate, record);
   }
 
-  private double calcCouponPayment(double cf, int years, double rate, HashMap<Integer, Double> memo) {
+  /**
+  * Calculates the value of coupon payments up to a certain year
+  * given the coupon rate, the current year, the face value,
+  * and the discount rate. Does this with recursive calls to get
+  * the values of previous years before updating the memo.
+  * (C / (1 + r)) + (C / (1 + r)^2) + ... + (C / (1 + r)^n)
+  * @param  coupon coupon rate
+  * @param  years  target year
+  * @param  face   face value
+  * @param  rate   discount rate
+  * @return        value coupon payments up until target year
+  */
+  private double calcCouponPaymentValue(double cf, int years, double rate, HashMap<Integer, Double> memo) {
+    /*
+     * Check if current year coupon payment was previously calculated.
+     * If not, calculate
+     */
     if (!memo.containsKey(years)) {
-      double previousCouponPayment = calcCouponPayment(cf, years - 1, rate, memo);
-      double currentCouponPayment = previousCouponPayment + cf / calcDiscount(years, rate);
+      double previousCouponPayment = calcCouponPaymentValue(cf, years - 1, rate, memo);
+      double currentCouponPayment = previousCouponPayment + cf / calcValueModifier(years, rate);
       memo.put(years, currentCouponPayment);
     }
+
     return memo.get(years);
   }
 
-  private double calcDiscount(int year, double rate) {
+  /**
+  * Calculates the modifier of coupon and principal payments
+  * C / (1 + r)^n
+  * @param  year  target year
+  * @param  rate  discount rate
+  * @return       value modifier for a given year
+  */
+  private double calcValueModifier(int year, double rate) {
     return Math.pow((1.0 + rate), year);
   }
 
-  private double calcDiscountHelper(int year, double rate, HashMap<Integer, Double> memo) {
-    if (!memo.containsKey(year)) {
-      double previousDiscount = calcDiscountHelper(year - 1, rate, memo);
-      double currentDiscount = previousDiscount * (1 + rate);
-      memo.put(year, currentDiscount);
-    }
-    return memo.get(year);
-  }
-
+  /**
+  * Calculates the coupon payment before discounted to present value
+  * C = cF
+  * @param  coupon coupon rate
+  * @param  face   face value
+  * @return        coupon payment
+  */
   private double calcCF(double coupon, double face) {
     return coupon * face;
   }
