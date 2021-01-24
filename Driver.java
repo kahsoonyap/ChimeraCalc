f50e9ac7847f76991399f1f0d204d19dd486e65f
Kah Soon Yap <kahsoonyap1998@hotmail.com>
2021-01-24 06:01:36 -0500

    randomized tests

--- Driver.java
+++ Driver.java
@@ -37,7 +37,28 @@ public class Driver {
     printResults(calc.CalcYield(0.10, 0, 500, 500.0), 0.0, 0.00001); // ?
     endTime = System.currentTimeMillis();
     timeDiff(startTime, endTime);
-    
+
+
+    System.out.println("======================= Automate =======================");
+    randomizedTests(5000, calc);
+    // calc.CalcYield(0.078680, 7, 48912.364471, 978454.949250);
+    // System.out.println(calc.calcPriceHelper(0.10, 5, 1000, -1.15));
+
+    // System.out.println(calc.calcPriceHelper(0.10, 5, 1000, -0.15));
+    // System.out.println(calc.CalcYield(0.10, 5, 1000, 3089.580147859961));
+    // System.out.println(calc.calcPriceHelper(0.10, 6, 1000, -0.15));
+    // System.out.println(calc.CalcYield(0.10, 6, 1000, 3752.447232776425));
+    // System.out.println(calc.calcPriceHelper(0.10, 6, 1000, -1.15));
+    // System.out.println(calc.calcPriceHelper(0.10, 6, 1000, -0.8493001111279779));
+    // System.out.println(calc.CalcYield(0.10, 6, 1000, 95425451.303315535));
+
+    // System.out.println(calc.CalcYield(0.948123, 62, 441301.903208, 21122.239053));
+    // System.out.println(calc.calcPriceHelper(0.948123, 62, 441301.903208, 19.80843));
+    // System.out.println(calc.calcPriceHelper(0.948123, 62, 441301.903208, -2.0169683933461027));
+
+    // System.out.println(calc.CalcYield(0.682333, 61, 989325.664565, 75064.401150));
+    // System.out.println(calc.CalcYield(0.591907, 86, 66494.903672, 920862.257603));
+    // System.out.println(calc.CalcYield(0.190328, 2, 130094.786234, 446515.333954));
   }
 
   public static void printResults(double result, double expected, double error) {
@@ -53,5 +74,50 @@ public class Driver {
 
   public static void timeDiff(long startTime, long endTime) {
     System.out.printf("Execution time: %dms\n\n", endTime - startTime);
+  }
+
+  public static void randomizedTests(int num, BondYieldCalculator calc) {
+    long startTime;
+    long endTime;
+    Random rand = new Random();
+
+    double coupon, face, rate, price;
+    int years;
+    boolean noErrors = true;
+
+    System.out.println("5000 CalcPrice calls on random values");
+    startTime = System.currentTimeMillis();
+    for (int n = 0; n < num; n++) {
+      coupon = rand.nextDouble();
+      years = rand.nextInt(100);
+      face = rand.nextDouble() * 1000000;
+      rate = rand.nextDouble() * 5;
+      calc.CalcPrice(coupon, years, face, rate);
+      // System.out.println(calc.CalcPrice(coupon, years, face, rate));
+    }
+    endTime = System.currentTimeMillis();
+    timeDiff(startTime, endTime);
+
+    System.out.println("5000 CalcYield calls on random values");
+    startTime = System.currentTimeMillis();
+    for (int n = 0; n < num; n++) {
+      coupon = rand.nextDouble();
+      years = rand.nextInt(100) + 1;
+      face = rand.nextDouble() * 1000000;
+      price = rand.nextDouble() * 1000000;
+      rate = calc.CalcYield(coupon, years, face, price);
+      double p = calc.calcPriceHelper(coupon, years, face, rate);
+      // System.out.printf("calc.CalcYield(%f, %d, %f, %f);\n", coupon, years, face, price);
+      // System.out.println(rate);
+      // System.out.println("" + p + " " + price);
+      noErrors = Math.abs(p - price) < 0.0000001 || rate == Double.NEGATIVE_INFINITY;
+      // System.out.println(n);
+      // System.out.printf("calc.CalcYield(%f, %d, %f, %f);\n", coupon, years, face, price);
+      if (!noErrors) {
+        System.out.println(rate);
+      }
+    }
+    endTime = System.currentTimeMillis();
+    timeDiff(startTime, endTime);
   }
 }
